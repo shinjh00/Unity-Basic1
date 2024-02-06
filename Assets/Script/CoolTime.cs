e381d250d8069e06e4dfc5fa14ae52a6b0867f2d
ShinJiHye <werytuiop00@gmail.com>
2024-02-06 15:56:17 +0900

    CoolTime

---
+++ Assets/Script/CoolTime.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoolTime : MonoBehaviour
+{
+    [SerializeField] Rigidbody rigid;
+    [SerializeField] float jumpCoolTime;
+    [SerializeField] float jumpPower;
+
+    private void Update()
+    {
+        jumpCoolTime += Time.deltaTime;
+
+        if (jumpCoolTime >= 3f && Input.GetKeyDown(KeyCode.Space))
+        {
+            rigid.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+            // ForceMode.Force : Áö±×½Ã ¹Ì´Â Èû
+            // ForceMode.Force : ÇÑ ¹ø Åö Ä¡´Â Èû
+
+            jumpCoolTime = 0f;
+        }
+        
+    }
+}
